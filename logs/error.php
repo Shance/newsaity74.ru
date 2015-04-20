f8f93efd0a7e3ade7e6a0a5e8fae5e5fe9b04ddb
shangin <shanginn@gmail.com>
2015-04-20 22:38:03 +0500

    Initial commit

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-04-20 12:26:14 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-04-20T12:26:14+00:00	INFO 37.140.67.55	joomlafailure	Имя пользователя и пароль не совпадают или у вас ещё нет учётной записи на сайте
+2015-04-20T12:26:18+00:00	INFO 37.140.67.55	joomlafailure	Имя пользователя и пароль не совпадают или у вас ещё нет учётной записи на сайте
